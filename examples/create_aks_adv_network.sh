9f72878138c3a431d182ca64e0e7072dbd5b5099
Tommy Falgout <tommy@lastcoolnameleft.com>
2018-11-07 09:37:34 -0600

    Rename AKS_DOCKER_BRIDGE_ADDRESS

--- examples/create_aks_adv_network.sh
+++ examples/create_aks_adv_network.sh
@@ -26,14 +26,14 @@ AKS_MAX_PODS=20
 
 AKS_SERVICE_CIDR=10.1.0.0/24          # Must be outside of Vnet Address space
 AKS_DNS_SERVICE_IP=10.1.0.5           # Must be inside the AKS Service CIDR
-AKS_DNS_BRIDGE_ADDRESS=172.17.0.1/16  # Must be outside of the Vnet Address space
+AKS_DOCKER_BRIDGE_ADDRESS=172.17.0.1/16  # Must be outside of the Vnet Address space
 
 az group create -n $VNET_RESOURCE_GROUP -l $LOCATION
 az network vnet create -g $VNET_RESOURCE_GROUP -n $VNET_NAME --address-prefix $VNET_ADDRESS_PREFIX
 az network vnet subnet create -g $VNET_RESOURCE_GROUP --vnet-name $VNET_NAME --address-prefix $VNET_ADDRESS_PREFIX -n $SUBNET_NAME
 
 az group create -n $AKS_RESOURCE_GROUP -l $LOCATION
-az aks create --resource-group $AKS_RESOURCE_GROUP --name $AKS_RESOURCE_GROUP --location $LOCATION --max-pods $AKS_MAX_PODS --admin-username $ADMIN_USERNAME --kubernetes-version $KUBERNETES_VERSION --network-plugin $AKS_NETWORK_PLUGIN --dns-name-prefix $DNS_NAME_PREFIX --node-count $AKS_NODE_COUNT --node-vm-size $AKS_NODE_VM_SIZE --vnet-subnet-id $VNET_SUBNET_ID --service-cidr $AKS_SERVICE_CIDR --dns-service-ip $AKS_DNS_SERVICE_IP --docker-bridge-address $AKS_DNS_BRIDGE_ADDRESS --enable-addons 'http_application_routing' --ssh-key-value ~/.ssh/id_rsa.pub --service-principal=$SERVICE_PRINCIPAL --client-secret=$SERVICE_PRINCIPAL_PASSWORD
+az aks create --resource-group $AKS_RESOURCE_GROUP --name $AKS_RESOURCE_GROUP --location $LOCATION --max-pods $AKS_MAX_PODS --admin-username $ADMIN_USERNAME --kubernetes-version $KUBERNETES_VERSION --network-plugin $AKS_NETWORK_PLUGIN --dns-name-prefix $DNS_NAME_PREFIX --node-count $AKS_NODE_COUNT --node-vm-size $AKS_NODE_VM_SIZE --vnet-subnet-id $VNET_SUBNET_ID --service-cidr $AKS_SERVICE_CIDR --dns-service-ip $AKS_DNS_SERVICE_IP --docker-bridge-address $AKS_DOCKER_BRIDGE_ADDRESS --enable-addons 'http_application_routing' --ssh-key-value ~/.ssh/id_rsa.pub --service-principal=$SERVICE_PRINCIPAL --client-secret=$SERVICE_PRINCIPAL_PASSWORD
 az aks get-credentials --resource-group $AKS_RESOURCE_GROUP --name $AKS_RESOURCE_GROUP
 
 # Cleanup
